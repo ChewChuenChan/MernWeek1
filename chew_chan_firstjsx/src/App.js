62d11ff5e181d0f7b316f54def3a8f32b382cf8a
Chew Chuen Chan <aliceccc@gmail.com>
2022-07-10 06:37:16 -0400

    added components

--- chew_chan_firstjsx/src/App.js
+++ chew_chan_firstjsx/src/App.js
@@ -1,17 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Title from './components/Title'
+import List from './components/List'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello Dojo!</h1>
-      <h3>Things I need to do:</h3>
-      <ul>
-        <li>Learn React</li>
-        <li>Climb Mt Everest</li>
-        <li>Run a marathon</li>
-        <li>Feed the dogs</li>
-      </ul>
+      <Title/>
+      <List/>
+      
     </div>
   );
 }
---
+++ chew_chan_firstjsx/src/components/List.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+const List = () => {
+    return(
+        <div>
+            <h3>Things I need to do:</h3>
+                <ul className='list'>
+                    <li>Learn React</li>
+                    <li>Climb Mt Everest</li>
+                    <li>Run a marathon</li>
+                    <li>Feed the dogs</li>
+                </ul>
+        </div>
+
+    )
+}
+
+export default List;
